e5b30abbb71e39b56eef376fdada68d5455b0433
shadeep0918 <shadeepisuru12345@gmail.com>
2026-02-09 17:43:01 +0530

    updated Calculator class

--- src/Calculator.java
+++ src/Calculator.java
@@ -5,5 +5,25 @@ import javax.swing.*;
 import javax.swing.border.LineBorder;
 
 public class Calculator {
+    int boardWidth =360;
+    int boardHeight =540;
+    Color customLightGray = new Color(212,212,210);
+    Color customDarkGray = new Color(80,80,80);
+    Color customBlack = new Color(28,28,28);
+    Color customOrange = new Color(255,149,0);
+
+
+    JFrame frame = new JFrame("Calculator");
+    JLabel displayJLabel = new JLabel();
+    JPanel disJPanel = new JPanel();
+
+    Calculator(){
+        frame.setVisible(true);
+        frame.setSize(boardWidth, boardHeight);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new BorderLayout());
+    }
 
 }
